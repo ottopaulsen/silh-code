32f80aa37d6b4acf413c6c8c8b7faf2e7d70fea1
Otto Paulsen <otto.paulsen@itema.no>
2017-12-11 23:08:00 +0100

    Set to prod

--- silh-code.php
+++ silh-code.php
@@ -5,7 +5,7 @@
  Description: Kode for SIL Håndball.
  Author: Otto Paulsen
  GitHub Plugin URI: https://github.com/ottopaulsen/silh-code
- GitHub Branch: master
+ GitHub Branch: prod
  Version: 2.0.27
  */
 
